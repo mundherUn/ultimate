fc1cf2108979f71fd1a6b7f3839e4ab035c5fa07
mundherUn <mundersaeed2@gmail.com>
2019-12-18 20:11:51 +0300

    upadtea 1

---
+++ dist/js/main-min.js
@@ -0,0 +1,1 @@
+$(function(){$(".toggle-sidebar").on("click",function(){$(".container-aera , .sidebar").toggleClass("no-sidebar")})});
---
+++ stage/js/main.js
@@ -0,0 +1,5 @@
+$(function() {
+    $(".toggle-sidebar").on("click", function() {
+        $(".container-aera , .sidebar").toggleClass("no-sidebar");
+    });
+});
